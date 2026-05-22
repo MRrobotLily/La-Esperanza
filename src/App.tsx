1e3317f8e5000159af207285a0407b07aa18ef90
Steve <ealvizsuresp5@miumg.edu.gt>
2026-05-21 22:32:18 -0600

    Agrego comentario en App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import AppProvider from './providers/AppProvider';
 import MainStack from './navigators/MainStack';
 
+// Componente principal donde se cargan los providers y la navegacion general del sistema.
 export default function App() {
   return (
     <AppProvider>
